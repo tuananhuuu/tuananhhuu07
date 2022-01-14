c4623f7b1d35d2ceba29d2962a385ca9e11330c2
tuananhuuu <90943165+tuananhuuu@users.noreply.github.com>
2022-01-14 10:44:07 +0700

    Add files via upload

---
+++ bai4.py
@@ -0,0 +1,10 @@
+# Lò Tuấn Anh
+# msv:1451020005
+ten = input(" Nhập tên và id sinh viên:  ")
+print(ten)
+n = len(ten)
+values=input(" chuỗi tạo ra ở câu a: ")
+l=values.split(",")
+t=tuple(l)
+print (l)
+print (t)
